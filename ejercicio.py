6ba85adb9df28733c6ffdf3aaaf1028af9e1356c
BenjaminCarrillo <ben.carrillo@duocuc.cl>
2025-07-01 14:58:40 -0400

    Add files via upload

--- ejercicio.py
+++ ejercicio.py
@@ -1,9 +1,95 @@
-def sumatoria():
-    contador=0
-    suma=0
-    while contador<15:
-        contador +=1
-        num=int(input(f"ingrese numero {contador}"))
-        suma +=num
-    print (suma)
-sumatoria()
+adventurers={
+} 
+esta=False
+def reg_adventurer(adventurers,codigo,nombre,edad):
+    if codigo not in adventurers:
+        adventurers[codigo]= {'nombre':nombre,'edad':edad,'puntaje':[]}
+    else:
+        print("ese codigo ya existe")
+def ing_puntaje(adventurers,codigo,puntaje):
+    adventurers[codigo]['puntaje'].append(puntaje) 
+def mod_puntaje(adventurers,codigo,sesion):
+    try:
+        puntaje=int(input("ingresa el nuevo puntaje"))
+    except:
+        try:
+            puntaje=int(input("ingresa el nuevo puntaje"))
+        except:
+            print("ingrese un numero valido")
+    adventurers[codigo]['puntaje'][sesion-1]=puntaje
+while True:
+    print("1. Registrar Aventurero.")
+    print("2. Registrar puntaje.")
+    print("3. Modificar un puntaje.")
+    print("4. total acumulado y promedio.")
+    print("5. Ver aventureros con bajo promedio.")
+    print("6. Lista de aventureros con datos y puntajes.")
+    print("7. aventureros por edad.")
+    print("8. SALIR.")
+    opcion=input("Ingresa una opcion= ")
+    if opcion=="1":
+        codigo=input("ingrese su codigo unico= ")
+        nombre=input("ingresa tu nombre= ")
+        try:
+            edad= int(input("ingrese su edad= "))
+        except:
+            while True:
+                try:
+                    edad=int(input("ingresa un numero valido= "))
+                    break
+                except:
+                    print("siga intentando")
+        reg_adventurer(adventurers,codigo,nombre,edad)
+    if opcion=="2":
+        codigo=input("ingresa tu codigo unico= ")
+        if codigo in adventurers:
+            esta==True
+            try:
+                puntaje=int(("ingrese su puntaje= "))
+            except:
+                while True:
+                    try:
+                        puntaje=int(input("ingrese el puntaje= "))
+                        break
+                    except:
+                        print("ingrese un numero= ")
+            ing_puntaje(adventurers,codigo,puntaje)
+        elif esta==False:
+            print("el usuario no existe")
+        lenght=len(adventurers[codigo]['puntaje']) 
+        print(f"{lenght} de longitud")
+    if opcion=="3":
+        codigo=input("ingrese codigo ")
+        if codigo in adventurers:
+            esta==True
+            try:
+                sesion=int(input("ingrese el numero de la sesion"))
+            except:
+                while True:
+                    try: 
+                        sesion=int(input("ingrese el numero de la sesion"))
+                    except:
+                        print("ingrese un numero")
+            lenght=len(adventurers[codigo]['puntaje']) 
+            if sesion<=lenght: 
+                mod_puntaje(adventurers,codigo,sesion)
+            else:
+                print("la sesion no existe")
+        elif esta==False:
+            print("el usuario no existe")
+    if opcion=="4":
+        for i in adventurers[codigo]:
+            for n in adventurers[codigo]['puntaje']:
+                print(i,n)
+    if opcion==5:
+        pass
+    if opcion==6:
+        pass
+    if opcion==7:
+        pass
+    if opcion==8:
+        pass
+    print (adventurers)
+    
+
+
